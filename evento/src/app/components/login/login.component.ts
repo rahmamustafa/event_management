cde681d9e69918b75f972126fb12ca01b890ec49
rahmamustafa <rahmamustafa169@gmail.com>
2023-06-17 22:34:09 +0300

    finish registration

--- evento/src/app/components/login/login.component.ts
+++ evento/src/app/components/login/login.component.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { CreateUserDto } from 'src/app/models/login/create-user-dto';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ApiService } from 'src/app/services/api.service';
 import { EmailValidator } from 'src/app/services/email-validator';
 
@@ -13,57 +13,59 @@ import { EmailValidator } from 'src/app/services/email-validator';
 })
 
 export class LoginComponent implements OnInit {
-  
-  constructor(private apiService:ApiService,private _formBuilder: FormBuilder 
-    
-    , private http: HttpClient , private emailValidator: EmailValidator){
+
+  constructor(private apiService: ApiService, private _formBuilder: FormBuilder
+
+    , private http: HttpClient, private emailValidator: EmailValidator, private _router: Router) {
 
   }
-  selectedCountry :string;
+  selectedCountry: string;
   selectedImage: File
-  email:string;
-  emailExists:boolean;
+  email: string;
+  emailExists: boolean;
   countries: any;
-  registerForm:FormGroup;
-  
+  registerForm: FormGroup;
+
 
   ngOnInit(): void {
-      this.http.get('https://trial.mobiscroll.com/content/countries.json').subscribe((resp: any) => {
-          const countries = [];
-          for (let i = 0; i < resp.length; ++i) {
-              const country = resp[i];
-              countries.push({ text: country.text, value: country.value });
-          }
-          this.countries = countries;
-      });
-      this.registerForm = this._formBuilder.group({
-        name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
-        password: ['', [Validators.required, Validators.minLength(5),Validators.maxLength(20)]],
-        email: ['', [Validators.required, Validators.email]],
-        phone: ['', [Validators.required,Validators.pattern("^[0][1][0125][0-9]{8}$")]], 
-        image: ['',[Validators.required]], 
-        country: ['',[Validators.required]], 
-        gender: ['',[Validators.required]], 
-        birthDate: ['',[Validators.required]] 
-      });
+    this.http.get('https://trial.mobiscroll.com/content/countries.json').subscribe((resp: any) => {
+      const countries = [];
+      for (let i = 0; i < resp.length; ++i) {
+        const country = resp[i];
+        countries.push({ text: country.text, value: country.value });
+      }
+      this.countries = countries;
+    });
+    this.registerForm = this._formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+      password: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(20)]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required, Validators.pattern("^[0][1][0125][0-9]{8}$")]],
+      image: ['', [Validators.required]],
+      country: ['', [Validators.required]],
+      gender: ['', [Validators.required]],
+      birthDate: ['', [Validators.required]]
+    });
 
   }
-  register(): void{
+  register(): void {
     this.registerForm.controls["country"].setValue(this.selectedCountry);
     const user = this.registerForm.value;
     console.log(user)
 
     const userData = new FormData();
     userData.append('image', this.selectedImage);
-    userData.append('user',JSON.stringify(user));
-    this.apiService.post("api/users",userData)
-    .subscribe({
-      next:response=>{
-        console.log(response)
-      },
-      error:error=>{}
-    }
-    );
+    userData.append('user', JSON.stringify(user));
+    this.apiService.post("api/users", userData)
+      .subscribe({
+        next: response => {
+          console.log(response)
+          this._router.navigateByUrl('/home');
+
+        },
+        error: error => { }
+      }
+      );
   }
 
   onChangeCountry(event: any) {
@@ -88,7 +90,11 @@ export class LoginComponent implements OnInit {
     }
     );
 }
-  
+
+  // validateEmail() {
+  //   this.emailExists = this.emailValidator.validateEmail(this.email);
+
+  //  }
 }
 
 
