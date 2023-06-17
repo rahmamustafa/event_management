46f41d8077f3b9cf9903e52afce51e5e607aab8f
Salma-Abdallah <119084967+Salma-Abdallah@users.noreply.github.com>
2023-06-17 09:54:51 +0300

    add the path

--- evento/src/app/app-routing.module.ts
+++ evento/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { EventComponent } from './components/event/event.component';
+import { EventListComponent } from './components/event-list/event-list.component';
 
 const routes: Routes = [
   {path:'home',component:HomeComponent},
   {path:'login',component:LoginComponent},
   {path:'event',component:EventComponent},
+  {path:'eventList',component:EventListComponent},
   {path:'',component:HomeComponent},
 
 ];
