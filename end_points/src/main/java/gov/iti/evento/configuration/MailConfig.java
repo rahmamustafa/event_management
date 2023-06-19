f0ba9d6abd36460afa748c32335c2c315b852ccc
mohgayasser <mohgayasserelsaid@gmail.com>
2023-06-19 16:18:37 +0300

    edit

--- end_points/src/main/java/gov/iti/evento/configuration/MailConfig.java
+++ end_points/src/main/java/gov/iti/evento/configuration/MailConfig.java
@@ -1,4 +1,4 @@
-package gov.iti.evento.configurations;
+package gov.iti.evento.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
