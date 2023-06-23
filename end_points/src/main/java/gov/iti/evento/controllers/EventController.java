db112a39d5f4b9de00b27e66f3ea4f83e52b087b
Salma-Abdallah <119084967+Salma-Abdallah@users.noreply.github.com>
2023-06-23 14:13:10 +0300

    add crossOrigin

--- end_points/src/main/java/gov/iti/evento/controllers/EventController.java
+++ end_points/src/main/java/gov/iti/evento/controllers/EventController.java
@@ -23,10 +23,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 import java.time.LocalDate;
@@ -36,6 +33,7 @@ import java.util.Optional;
 import java.util.stream.Stream;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class EventController {
     @Autowired
     CategoryRepository categoryRepository;
--- end_points/src/main/java/gov/iti/evento/controllers/UserController.java
+++ end_points/src/main/java/gov/iti/evento/controllers/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
     @Autowired
